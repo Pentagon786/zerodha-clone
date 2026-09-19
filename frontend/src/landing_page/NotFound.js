87e5b8bc47d44f0aeb3810a711f02b0061b6ff0c
spidelux_BT <Pentagon40BT@gmail.com>
2026-09-19 17:01:45 +0530

    Add frontend source files

---
+++ frontend/src/landing_page/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function NotFound() {
+    return ( 
+        <div className='container p-5 mb-5'>
+          <div className='row text-center' >
+            <h1 className='mt-5'>505 Not Found</h1>
+            <p>We couldn’t find the page you were looking for. Visit Zerodha’s home page</p>
+          </div>
+        
+       </div>
+     );
+}
+
+export default NotFound;
---
+++ frontend/src/landing_page/support/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+function Hero() {
+    return ( 
+        <section className='container 'id="supportHero">
+            <div className='p-5 mt-5' id='supportWrapper'>
+                <h4>Support Portal</h4 >
+                <a href="">Track Tickets </a>
+            </div>
+             <div className='row p-5 mt-5' >
+             <div className=' col-6 row p-5 mt-5' ></div>
+                <h4>Support Portal</h4 >
+                <a href="">Track Tickets </a>
+            </div>
+        </section>
+     );
+}
+
+export default Hero;
